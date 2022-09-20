899cd0c45c5ea27d51d552af1c867dcc77ac49b8
lneff <lneff@APT02-6Y2PGK3.SWG.cdc.root>
2022-09-20 09:54:34 +0200

    basisversion

---
+++ Übungsaufgabe/src/Aufagabe_9_1.java
@@ -0,0 +1,15 @@
+
+public class Aufagabe_9_1 {
+
+	public static void main(String[] args) {
+		double n = 3;
+		double i = 3;
+		
+		while(i <2*n) {
+			i++;
+			System.out.print(3.14/(2*i-1));
+		}
+
+	}
+
+}
